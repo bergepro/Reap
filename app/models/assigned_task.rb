0ee3b498418e5ce2f657496647ebe2fe1b55aa16
Sondre Alvestad <sondresalvestad@gmail.com>
2023-03-31 22:15:57 +0200

    Models, migrations and views for tasks (not completed)

---
+++ app/models/assigned_task.rb
@@ -0,0 +1,3 @@
+class AssignedTask < ApplicationRecord
+  
+end
---
+++ db/migrate/20230331200450_create_assigned_tasks.rb
@@ -0,0 +1,8 @@
+class CreateAssignedTasks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :assigned_tasks do |t|
+
+      t.timestamps
+    end
+  end
+end
