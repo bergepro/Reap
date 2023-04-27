35f66fe18c988b9c7542a428c334c001a5da8c42
lund <christofferpaintball1@gmail.com>
2023-04-27 22:20:26 +0200

    box fits hover navbar

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -13,6 +13,9 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      minWidth: {
+        '6': '6em',
+      },
     },
   },
   plugins: [
