c3f88de3d3837f298d5273466b4f874a7f5b8853
Sondre Alvestad <sondresalvestad@gmail.com>
2023-05-07 14:02:59 +0200

    Nesten komplett import-export implementasjon

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
-  validates :notes, format: { without: /\r|\n/, message: "Line breaks are not allowed" }
+  validates :name, format: { without: /\r|\n/, message: "Line breaks are not allowed" }
 
   has_many :assigned_tasks
 end
