d117a37e3013ae542878fdbe410f069def6668db
Morten Berge <morten@berge.pro>
2023-05-08 17:11:31 +0200

    design

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
   def index
+    @time_regs = current_user.time_regs.order('time_regs.date_worked DESC', 'time_regs.assigned_task_id')
   end
 end
