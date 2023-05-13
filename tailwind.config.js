e3104256829031cf30ed06653a755cc5660f5fa4
Morten Berge <morten@berge.pro>
2023-05-13 15:24:19 +0200

    navbar responsive layout, custom theme colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,23 @@
 module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'tastyWhite': '#F4EDE3',
+        'cream': '#F0E7DB',
+      },
+      minWidth: {
+        '6': '6em',
+        'custom50': '50rem',
+        'custom45': '45rem',
+        'custom35': '35rem',
+        'custom25': '25rem',
+      },
+      width: {
+        'custom20': '20rem',
+        'custom15': '15rem',
+      },
+    },
+  },
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
