75bb2e0a91a761de9eb045f02d6aa4cdc1296cf1
lund <christofferpaintball1@gmail.com>
2023-05-16 09:51:43 +0200

    collapsehours.js commented, timetask.js commented

--- app/javascript/controllers/collapse_hours_controller.js
+++ app/javascript/controllers/collapse_hours_controller.js
@@ -1,16 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="collapse-hours"
-
 export default class extends Controller {
+  // creates a eventlistener on every button on every task in the project page
   connect() {
-    let collapsibles = document.getElementsByClassName('collapsible');
+    let collapsibles = document.getElementsByClassName('collapsible'); // each button
   
+    // iterates over every task collapsible div
     for(let i = 0; i < collapsibles.length; i++){
       let button = collapsibles[i];
       let collapsibleId = "collapsible-" + button.dataset.taskId;
       let collapsible = document.getElementById(collapsibleId)
   
+      // toggles a collapse div visibility with everyones hours
       button.addEventListener('click', () => {
           collapsible.classList.toggle('hidden');
       });
