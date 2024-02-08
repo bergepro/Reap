0a1b2d990eadf2b0050b462fc2d3f182da6a6fee
WildCard-NO <kalex.boe@gmail.com>
2024-02-08 09:38:06 +0100

    convert .css to tailwind

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,44 +1,57 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
   ],
   theme: {
     extend: {
       colors: {
-        'tastyWhite': '#F4EDE3',
-        'tastyWhiteLite': '#F9F6F1',
-        'cream': '#F0E7DB',
-        'seaGreen': '#319895',
-        'seaGreenDark': "#285E61",
-        'seaGreenDarker': "#193c3e",
-        'dangerRed': "#ff0000",
+        tastyWhite: "#F4EDE3",
+        tastyWhiteLite: "#F9F6F1",
+        cream: "#F0E7DB",
+        seaGreen: "#319895",
+        seaGreenDark: "#285E61",
+        seaGreenDarker: "#193c3e",
+        dangerRed: "#ff0000",
+
+        lightsalmon: "#FFA07A",
+        orange: "#FFA500",
       },
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
       minWidth: {
-        '6': '6em',
-        'custom60': '60rem',
-        'custom50': '50rem',
-        'custom45': '45rem',
-        'custom35': '35rem',
-        'custom25': '25rem',
+        6: "6em",
+        custom60: "60rem",
+        custom50: "50rem",
+        custom45: "45rem",
+        custom35: "35rem",
+        custom25: "25rem",
+        custom24: "24rem",
       },
       width: {
-        'custom20': '20rem',
-        'custom15': '15rem',
+        custom20: "20rem",
+        custom15: "15rem",
       },
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-  ]
-}
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+    function ({ addUtilities }) {
+      const newUtilities = {
+        ".border-2-black": {
+          "border-width": "2px",
+          "border-color": "#000",
+        },
+      };
+
+      addUtilities(newUtilities, ["responsive", "hover"]);
+    },
+  ],
+};
